cbf40de0370d23517e20338ef6d687915c966296
root <root@ip-172-31-24-176.us-east-2.compute.internal>
2018-01-08 20:01:47 +0000

    src

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w,"Hello")
+}
+
+
+func main() {
+    fmt.Println("Starting server")
+    http.HandleFunc("/", HelloWorld)
+    http.ListenAndServe(":8081",nil)
+}
